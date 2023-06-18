0f1d0601a705085b176142401dd2ff2afc070922
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-06-18 17:46:10 +0300

    Project was created

---
+++ JobApp/app/index.js
@@ -0,0 +1,15 @@
+import { Text, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter} from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+
+const Home = () => {
+    return(
+        <ScrollView>
+            <Text>Homes</Text>
+        </ScrollView>
+    )
+}
+
+export default Home;
